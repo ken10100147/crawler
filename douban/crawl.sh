e5e9e18443c446759c90c2caa8bbe23642a350ff
ken <sunhongping@kanjian.com>
2017-12-06 14:51:00 +0800

    douban: adjust

--- douban/crawl.sh
+++ douban/crawl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scrapy crawl music
+#scrapy crawl music
 scrapy crawl review
 scrapy crawl follower
 scrapy crawl user -s DOWNLOAD_DELAY=4 -s CONCURRENT_REQUESTS_PER_DOMAIN=1
--- zhihu/crawl.sh
+++ zhihu/crawl.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-scrapy crawl generalsearch
+#scrapy crawl generalsearch
 scrapy crawl answer
 scrapy crawl topic
 scrapy crawl topic_follower
