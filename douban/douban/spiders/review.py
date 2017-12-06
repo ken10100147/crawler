3b8d940b9db9e6d5546cb000b6b0037ade6da14c
ken <sunhongping@kanjian.com>
2017-12-06 17:09:30 +0800

    douban: adjust

--- douban/douban/spiders/review.py
+++ douban/douban/spiders/review.py
@@ -20,6 +20,31 @@ class ReviewSpider(scrapy.Spider):
             yield request
 
     def parse(self, response):
+        # review_list = response.xpath('//div[@class="review-list  "]/div')
+        # for review in review_list:
+        #     reply = review.xpath('.//div[@class="action"]/a[@class="reply"]/text()').extract_first(default='')
+        #     reply = reply[0:(len(reply) - 2)]
+        #
+        #     yield Review(id=review.xpath('@data-cid').extract_first(),
+        #                  music_id=response.meta['music'].id,
+        #                  title=review.xpath('.//div[@class="main-bd"]/h2/a/text()').extract_first(default=''),
+        #                  summary=review.xpath('.//div[@class="short-content"]/text()').extract_first(
+        #                      default='').strip(),
+        #                  comments=reply,
+        #                  useful_count=review.xpath(
+        #                      './/div[@class="action"]/a[contains(@class,"up")]/span/text()').extract_first(
+        #                      default='0').strip(),
+        #                  useless_count=review.xpath(
+        #                      './/div[@class="action"]/a[contains(@class,"down")]/span/text()').extract_first(
+        #                      default='0').strip())
+        #
+        count = int(response.xpath('//h1/text()').re_first('([0-9]{1,})'))
+        for i in range(0, count, 35):
+            request = scrapy.Request(response.url + '?start=' + str(i), callback=self.parse_review)
+            request.meta['music'] = response.meta['music']
+            yield request
+
+    def parse_review(self, response):
         review_list = response.xpath('//div[@class="review-list  "]/div')
         for review in review_list:
             reply = review.xpath('.//div[@class="action"]/a[@class="reply"]/text()').extract_first(default='')
@@ -37,12 +62,6 @@ class ReviewSpider(scrapy.Spider):
                          useless_count=review.xpath(
                              './/div[@class="action"]/a[contains(@class,"down")]/span/text()').extract_first(
                              default='0').strip())
-
-        count = int(response.xpath('//h1/text()').re_first('([0-9]{1,})'))
-        for i in range(0, count, 35):
-            request = scrapy.Request(response.url + '?start=' + str(i))
-            request.meta['music'] = response.meta['music']
-            yield request
 
             # def parse_review(self, response):
             #     review_list = response.xpath('//div[@class="review-list  "]/div')
