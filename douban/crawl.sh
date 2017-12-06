0a388e47c3cb20244ceaf22d2e2fa8737eff8c62
ken <sunhongping@kanjian.com>
2017-12-06 18:28:58 +0800

    zhihu: adjust

--- douban/crawl.sh
+++ douban/crawl.sh
@@ -4,3 +4,7 @@
 scrapy crawl review
 scrapy crawl follower
 scrapy crawl user -s DOWNLOAD_DELAY=4 -s CONCURRENT_REQUESTS_PER_DOMAIN=1
+#run multi time to get more info
+scrapy crawl user -s DOWNLOAD_DELAY=4 -s CONCURRENT_REQUESTS_PER_DOMAIN=1
+#run multi time to get more info
+scrapy crawl user -s DOWNLOAD_DELAY=4 -s CONCURRENT_REQUESTS_PER_DOMAIN=1
